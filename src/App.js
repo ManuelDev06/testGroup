a21a5461cc7340630f3606f29701bb8f36e12fb3
ManuelDev06 <manuel.barba@improving.com>
2022-09-22 13:08:33 -0500

    ultimos camios

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Input from './components/Input';
 import Label from './components/Label';
 
 function App() {
+  // You have show the the value of the input in the label whe you push the button
   return (
     <div>
      <Input/>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import '../theme/Button.css'
 
 const Button = (text, onClick) => {
-  //Button need to receive the event and the text of the button
+    
   return (
     <div>Button</div>
   )
